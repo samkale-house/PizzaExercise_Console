9c5aefed7236e2aa8314252da536faa3f294d977
SamGrishma <sanvedanakale@gmail.com>
2020-06-30 23:10:24 -0500

    Editing comment

--- PizzaExercise_Console/Data/PizzaRepo.cs
+++ PizzaExercise_Console/Data/PizzaRepo.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace PizzaExercise_Console.Data
 {
     /// <summary>
-    /// This class provides methods to interact and query with Json data and provide result
+    /// This class provides methods to interact with and query Json data and provide result
     /// </summary>
     public class PizzaRepo
     {
